2eaa9e8e273d3ddf1efaf7c033f3119b0278629b
Xavystarr07 <xavierchetty07@gmail.com>
2026-03-14 19:18:17 +0200

    Working on collection page

---
+++ public/js/core/vault-ripple.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+      "user strict"
+      $('.water-effect').ripples({
+      resolution: 256,
+        dropRadius: 20,
+        perturbance: 0.04,
+        interactive:true
+      });
+});
